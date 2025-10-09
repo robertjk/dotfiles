f5801011fea6d7623b84d2859521f71cd8ef4a27
Robert J. Kusznier <no-cheating@autistici.org>
2025-10-09 21:11:16 +0200

    Update wezterm colors

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -2,7 +2,13 @@ local wezterm = require "wezterm"
 
 local config = wezterm.config_builder()
 
-config.color_scheme = "Jellybeans"
 config.font = wezterm.font "Cascadia Code"
+
+config.color_scheme = "Jellybeans (Gogh)" -- Alternatively "Jellybeans"
+config.colors = {
+    scrollbar_thumb = "#404040"
+}
+
+config.enable_scroll_bar = true
 
 return config
